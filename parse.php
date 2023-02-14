e5a15b3259a48a0d999e541fe8ea36ccc38f5437
RomanFilip <xroman16@vutbr.cz>
2023-02-14 17:09:57 +0100

    finished printing instructions

--- parse.php
+++ parse.php
@@ -2,6 +2,14 @@
 ini_set('display_errors', 'stderr');
 
 
+define("SUCCESS", 0);
+define("ERROR_MISSING_PARAMETER", 10);
+define("ERROR_OPEN_FILE", 12);
+define("ERROR_INTERNAL", 99);
+define("ERROR_MISSING_HEADER", 21);
+define("ERROR_OPERATING_CODE", 22);
+define("ERROR_LEX_SYNTAX", 23);
+
 /**
  * print instruction and save instruction order
  */
@@ -10,8 +18,45 @@ function printInstruction($order, $instruction) {
     return $order + 1;
 }
 
-function printArguments($argument, $order) {
-    echo("\t\t<arg".$order." type=\"var\">$argument</arg".$order.">\n");
+function printArguments($argument, $order, $isLabel=false) {
+    if($isLabel) {
+        echo("\t\t<arg".$order." type=\"label\">$argument</arg".$order.">\n");
+        return;
+    }
+
+    if(!strpos($argument, '@')) {
+        $type = 'type';
+        echo("\t\t<arg".$order." type=\"".$type."\">$argument</arg".$order.">\n");
+        return;
+    }
+    $operation = explode('@', $argument);
+    switch($operation[0]) {
+        case 'int':
+            $type = "int";
+            $argument = $operation[1];
+            break;
+        case 'bool':
+            $type = "bool";
+            $argument = $operation[1];
+            break;
+        case 'string':
+            $type = "string";
+            $argument = $operation[1];
+            break;
+        case 'nil':
+            $type = "nil";
+            $argument = $operation[1];
+            break;
+        case 'GF':
+        case 'LF':
+        case 'TF':
+            $type = "var";
+            $argument = $argument;
+            break;
+        default:
+            exit(ERROR_LEX_SYNTAX);
+    }
+    echo("\t\t<arg".$order." type=\"".$type."\">$argument</arg".$order.">\n");
 }
 
 // spracovanie parametrov
@@ -19,13 +64,15 @@ if ($argc > 1){
     if ($argv[1] == "--help") {
         echo("Skript typu filtr nacita za standartneho vstupu zdrojovy kod v IPPcode23, zkontroluje lexikalnu
         a syntakticku spravnost kodu a vypise na standartny vystup XML reprezentace programu.\n");
-        exit(0);
+        exit(SUCCESS);
     }
-    exit(10);
+    exit(ERROR_MISSING_PARAMETER);
 } 
  
 $order = 1;
 $header = false;
+$variables = [];
+// $labelList = [];
 
 #### TODO
 /*
@@ -42,12 +89,17 @@ while($line = fgets(STDIN)) {
         echo("<program language=\"IPPcode23\">\n");
     } 
     if ($header == false) {
-        exit(21); # chybna hlavicka;
+        exit(ERROR_MISSING_HEADER); # chybna hlavicka;
     }
     // check trim
     // oddelit komentar strip
     // echo($splitted_line[0])."\n";
     // echo count($splitted_line)."\n";
+
+    #####  ????????
+    // $line = preg_replace('/#.*/', '', $line);
+    // $line = preg_replace('/\s+/', ' ', $line);
+
 
     switch(strtoupper($splitted_line[0])) {
         // 1 var
@@ -56,20 +108,21 @@ while($line = fgets(STDIN)) {
             $order = printInstruction($order, $splitted_line[0]);
             if (preg_match("/(Lf|GF|TF)@[a-zA-Z#&*$][a-zA-Z#&*$0-9]*/", $splitted_line[1])) {
                 printArguments($splitted_line[1], 1);
+                array_push($variables, $splitted_line[1]);
                 // echo("\t\t<arg1 type=\"var\">$splitted_line[1]</arg1>\n");
             } else {
-                exit(23);
+                exit(ERROR_LEX_SYNTAX);
             }
-            echo("\t</instruction>\n");
             break;
         // label
-        case 'CALL':
         case 'LABEL':
+            // array_push($labelList, $splitted_line[1]);
+        case 'CALL':
         case 'JUMP':
+            // if (!array_search($splitted_line[1], $labelList)) exit(ERROR_LEX_SYNTAX);
             $order = printInstruction($order, $splitted_line[0]);
-            printArguments($splitted_line[1], 1);
+            printArguments($splitted_line[1], 1, true);
             // check if is defined?
-            echo("\t</instruction>\n");
             break;
         case 'MOVE':
             // check if is something after
@@ -79,21 +132,21 @@ while($line = fgets(STDIN)) {
             // }
             $order = printInstruction($order, $splitted_line[0]);
             if (preg_match("/(Lf|GF|TF)@[a-zA-Z#&*$][a-zA-Z#&*$0-9]*|int@[-+0-9][0-9]*|string@[a-zA-Z][a-zA-Z]*|nil@nil|bool@(true|false)/", $splitted_line[1])) {
-                printArguments($splitted_line[1], 1);
                 if (preg_match("/(Lf|GF|TF)@[a-zA-Z#&*$][a-zA-Z#&*$0-9]*|int@[-+0-9][0-9]*|string@[a-zA-Z][a-zA-Z]*|nil@nil|bool@(true|false)/", $splitted_line[2])) {
-                    // printArguments($splitted_line[2], 2);
-                    $newstring = substr($splitted_line[2], -3);
-                    echo("\t\t<arg2 type=\"int\">".$newstring."</arg2>\n");
-                }
-            } 
-            echo("\t</instruction>\n");
+                    // if (!array_search($splitted_line[1], $variables)) exit(ERROR_LEX_SYNTAX);
+                    printArguments($splitted_line[1], 1);
+                    printArguments($splitted_line[2], 2);
+                    // $newstring = substr($splitted_line[2], -3);
+                    // echo("\t\t<arg2 type=\"int\">".$newstring."</arg2>\n");
+                } else exit(ERROR_LEX_SYNTAX);
+            } else exit(ERROR_LEX_SYNTAX);
             break;
         case 'PUSHS':
             $order = printInstruction($order, $splitted_line[0]);
             if (preg_match("/(Lf|GF|TF)@[a-zA-Z#&*$][a-zA-Z#&*$0-9]*|int@[-+0-9][0-9]*|string@[a-zA-Z][a-zA-Z]*|nil@nil|bool@(true|false)/", $splitted_line[1])) {
                 printArguments($splitted_line[1], 1);
-                echo("\t</instruction>\n");
-            }
+
+            } else exit(ERROR_LEX_SYNTAX);
             break;
         // var symb1 symb2
         case 'ADD':
@@ -106,8 +159,7 @@ while($line = fgets(STDIN)) {
                 printInstruction($splitted_line[1], 1);
                 printInstruction($splitted_line[2], 2);
                 printInstruction($splitted_line[3], 3);
-            }
-            echo("\t</instruction>\n");
+            } exit(ERROR_LEX_SYNTAX);
             break;
         case 'IDIV':
         case 'LT':
@@ -116,22 +168,28 @@ while($line = fgets(STDIN)) {
         case 'AND':
         case 'OR':
         case 'NOT':
+        case 'CONCAT':
         case 'STRI2INT':
+
+        case 'SETCHAR':
+        case 'GETCHAR':
             $order = printInstruction($order, $splitted_line[0]);
-            printInstruction($splitted_line[1], 1);
-            printInstruction($splitted_line[2], 2);
-            printInstruction($splitted_line[3], 3);
-            echo("\t</instruction>\n");
+            printArguments($splitted_line[1], 1);
+            printArguments($splitted_line[2], 2);
+            break;
         //
+        case 'TYPE':
+        case 'STRLEN':
         case 'INT2CHAR':
         case 'READ':
-        case 'CONCAT':
-        case 'STRLEN':
-        case 'GETCHAR':
-        case 'SETCHAR':
-        case 'TYPE':
+            $order = printInstruction($order, $splitted_line[0]);
+            printArguments($splitted_line[1], 1);
+            break;
         case 'JUMPIFEQ':            
         case 'JUMPIFNEQ': 
+            $order = printInstruction($order, $splitted_line[0]);
+            printArguments($splitted_line[1], 1, true);
+            break;
         //  
         case 'WRITE': // todo
         case 'EXIT':
@@ -142,11 +200,18 @@ while($line = fgets(STDIN)) {
         case 'RETURN':
         case 'BREAK':
             $order = printInstruction($order, $splitted_line[0]);
-            echo("\t</instruction>\n");
             break;
+        case '#':
+        case '.IPPCODE23':
+            break;
+        default:
+            echo "something is wronge ";
+            echo $splitted_line[0];
+            exit(ERROR_OPERATING_CODE);
     }
+    if($splitted_line[0] != '.IPPcode23') echo("\t</instruction>\n");
 }
 echo("</program>\n");
-exit(0);
+exit(SUCCESS);
 
 ?>
